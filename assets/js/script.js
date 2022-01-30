f71d804eeedb9ae5787dbab350a5df24a4f78337
unknown <moutimg@gmail.com>
2022-01-30 15:02:51 -0300

    parcial

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 // Funcoes para link de criar conta
 const btnCreate = document.querySelector("#btn-criar-conta");
+const btnLogin = document.querySelector('.btn-login');
 const formRegister = document.querySelector('.form-register');
 const formLogin = document.querySelector('.form-login');
 const showRegisterForm = (event) => {
